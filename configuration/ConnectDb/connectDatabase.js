a545c39edf81144519665ecd90a8bd0b4ddc9997
amare <amarehagos26273424@gmail.com>
2024-04-08 13:31:47 +0300

    useNewUrlParser and useUnifiedTopology

--- configuration/ConnectDb/connectDatabase.js
+++ configuration/ConnectDb/connectDatabase.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose")
 
 const connectDatabase = () => {
   mongoose
-    .connect('mongodb+srv://amarehagos26273424:26273424@mernproject.ww2aaqx.mongodb.net/resultdb?retryWrites=true&w=majority', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect('mongodb+srv://amarehagos26273424:26273424@mernproject.ww2aaqx.mongodb.net/resultdb?retryWrites=true&w=majority')
     .then((data) => {
       console.log(`mongod connected with server: ${data.connection.host}`);
     });
